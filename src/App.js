05a197f87c274dbc20261d50d59a4060a3cf56f2
ziLun19 <b0829049@cgu.edu.tw>
2021-05-11 21:05:33 +0800

    new

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Hw4 from './hw4'
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+      { Hw4() }
+      </div>
+    </div>
+  );
+}
+export default App;
